e65e89ed7eec48fd65f5b840202f4c614dd27ebf
Aaron Brendel <aaron@brendel.org>
2017-12-10 21:23:13 -0500

    commented MongoDB recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,8 +2,12 @@
 # Cookbook:: MongoDB
 # Recipe:: default
 #
-# Copyright:: 2017, The Authors, All Rights Reserved.
+# Copyright:: 2017, Aaron Brendel, All Rights Reserved. But I really don't care if you want to use this code for whatever purpose you see fit. Enjoy! 
+#
+#
 
+
+# Add the MongoDB repository to Yum 
 yum_repository 'MongoDB' do
 	description "MongoDB Repository"
 	baseurl "http://downloads-distro.mongodb.org/repo/redhat/os/x86_64/"
@@ -11,8 +15,10 @@ yum_repository 'MongoDB' do
 	action :create
 end
 
+# Install the mongodb-org package from the repository added above.
 package 'mongodb-org'
 
+# Ensure that the services is started and will start upon reboot.
 service 'mongod' do
 	action [:enable, :start]
 end
